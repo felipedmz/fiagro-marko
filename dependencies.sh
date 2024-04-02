d62802232639fbeb6758b09b94a671f008882e19
Felipe Maziero <flpmaziero@gmail.com>
2024-04-01 23:23:34 -0300

    chore(save): decoupling save from weights calculation

---
+++ dependencies.sh
@@ -0,0 +1,5 @@
+pip install PyPortfolioOpt
+conda install -c conda-forge pandas numpy matplotlib scikit-learn pytorch
+conda list -e > requirements.txt
+conda install --file requirements.txt
+
--- optimizer.py
+++ optimizer.py
@@ -1,36 +1,38 @@
 import sys
 import time
 import pandas as pd
+from datetime import datetime
 from pypfopt.efficient_frontier import EfficientFrontier
 from pypfopt.expected_returns import mean_historical_return
 from pypfopt.risk_models import CovarianceShrinkage
 
 
 def calculate(file_path):
-    # Lendo o arquivo CSV
     df = pd.read_csv(file_path, sep=";", quotechar='"', index_col=0)
-
-    # Calculando os retornos esperados e a matriz de covariância
+    # calculando os retornos esperados e a matriz de covariância
     retornos_esperados = mean_historical_return(df)
     matriz_covariancia = CovarianceShrinkage(df).ledoit_wolf()
-
-    # Otimizando para o máximo Sharpe Ratio
+    # otimizando para o máximo Sharpe Ratio
     ef = EfficientFrontier(retornos_esperados, matriz_covariancia)
     pesos = ef.max_sharpe()
+    #print('... pesos', pesos)
     pesos_limpos = ef.clean_weights()
-
-    # Convertendo os pesos para porcentagens e salvando em um DataFrame
+    #print('... pesos_limpos', pesos)
+    # convertendo os pesos para porcentagens e salvando em um DataFrame
     pesos_porcentagem = {k: v * 100 for k, v in pesos_limpos.items()}
     df_pesos = pd.DataFrame(list(pesos_porcentagem.items()), columns=['Fundo', 'Alocação (%)'])
-
-    # Salvando os pesos otimizados em um arquivo CSV
-    df_pesos.to_csv("alocacao_optima.csv", index=False)
+    return df_pesos
 #
 def main():
     now = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
     print(f'> fiagro.markowitz - started={now}')
     start_time = time.time()
-    calculate(sys.argv[1])
+    print(f'... starting markowitz optimized allocation')
+    result = calculate(sys.argv[1])
+    print(f'... best allocation found', result)
+    output_file_name = "alocacao_optima.csv"
+    result.to_csv(output_file_name, index=False)
+    print(f'... best allocation found::: saved in `{output_file_name}`')
     end_time = time.time()
     elapsed_time = end_time - start_time
     print(f'> end - elapsed time={elapsed_time}')
